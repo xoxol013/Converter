3bfaed55188c5c019b5af227a189ccac2ec2de95
Petro_Krupko <bmxgtnz@gmail.com>
2025-06-26 04:47:16 +0200

    rename branch master

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,7 +5,8 @@ public class Main {
     private static final double CONV_K = 2.20462;
 
     public static void main(String[] args) {
-        System.out.println("Converter App. ");
+
+        System.out.println("App for measures converting. ");
 
         double kgs = 5;
         double pounds = convKgsToPounds(ksg);
